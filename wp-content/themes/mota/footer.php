e5c964b54bf89b39e3537e344cd7f8e82a8a521b
Simon Commereuc <simon.commereuc@gmail.com>
2024-03-26 14:05:23 +0100

    Footer terminé

---
+++ wp-content/themes/mota/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <nav>
+        <div class="footer_menu">
+            <ul>
+                <li><a href="#" class="footer_link">Mentions Légales</a></li>
+                <li><a href="#" class="footer_link">Vie Privée</a></li>
+                <li><a href="#" class="footer_link">Tout Droits Réservés</a></li>
+            </ul>
+        </div>
+    </nav>
+</footer>
