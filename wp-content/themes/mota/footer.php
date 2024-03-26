6ef3a31b5019b3aa28a5515f73ab0eca422e2210
Simon Commereuc <simon.commereuc@gmail.com>
2024-03-26 14:11:04 +0100

    Footer corrigé et nom des fichiers modifiés

--- wp-content/themes/mota/footer.php
+++ wp-content/themes/mota/footer.php
@@ -4,7 +4,7 @@
             <ul>
                 <li><a href="#" class="footer_link">Mentions Légales</a></li>
                 <li><a href="#" class="footer_link">Vie Privée</a></li>
-                <li><a href="#" class="footer_link">Tout Droits Réservés</a></li>
+                <li><p class="footer_text">TOUT DROITS RÉSERVÉS</p></li>
             </ul>
         </div>
     </nav>
